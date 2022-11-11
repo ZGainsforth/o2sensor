ac8886a2d62e18354fd59b017c9cb31d6a96edf3
Zack Gainsforth <zsg@gainsforth.com>
2022-11-10 22:50:01 -0800

    Use ring buffer for averaging last minute of O2 readings.  Comments. Temp correction for O2 reading.

--- o2sensor.c
+++ o2sensor.c
@@ -10,16 +10,19 @@
 #include "hardware/adc.h"
 #include "oled_i2c.h"
 #include "bmp280_i2c.h"
+#include <string.h>
+#include <math.h>
 
 #define XISTOR_PIN 6 
-#define ZEROSEQUENCELENGTH 100
+#define ADCSEQUENCELENGTH 60 
 #define VOLTSPERPERCENTO2 0.05  //This is the default for a SGX-40X -- tune the pot to make it perfect.
 
-uint16_t mean(uint16_t* x, uint16_t lenx){
+float mean(uint16_t* x, uint16_t lenx){
     uint16_t i;
-    uint16_t meanval = 0;
+    /* uint16_t meanval = 0; */
+    float meanval=0;
     for(i=0; i<lenx; i++){
-        meanval += x[i];
+        meanval += (float)x[i];
     }
     meanval = meanval/lenx;
     return meanval;
@@ -28,17 +31,31 @@ uint16_t mean(uint16_t* x, uint16_t lenx){
 int main() {
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
-    uint16_t result = 0;
-    uint16_t zerolevel = 0;
-    uint16_t zerolevelsequence[ZEROSEQUENCELENGTH];
-    char display_str[100] = "\n";
-    int i;
+    // Readout from ADC from O2 sensor.
+    float result = 0;
+    // Readout from ADC when O2 sensor disconnected by MOSFET.
+    float zerolevel = 0;
+    // Readouts are the aveage of adcsequence measurements
+    uint16_t adcsequence[ADCSEQUENCELENGTH];
+    // It's a ring buffer.
+    uint16_t adcposition;
+    // voltage is calculated from result and zerolevel.
     float voltage = 0;
+    // O2Pct is calculated from voltage.
     float O2pct = 0;
+    // tempcorrection is used to calculate the difference in the O2 reading as a function of temp.
+    float tempcorrection = 0;
+    // To hold strings to output to the oled.
+    char display_str[100] = "\n";
+    // For loops.
+    int i;
 
 
+    // Initialize 
     stdio_init_all();
     printf("O2 sensor booting.\n");
+    
+    // Init ADC to measure O2 sensor.
     printf("ADC measuring on GPIO26 (board pin 31).\n");
     adc_init();
     // Make sure GPIO is high-impedance, no pullups etc
@@ -46,56 +63,59 @@ int main() {
     // Select ADC input 0 (GPIO26)
     adc_select_input(0);
     
+    // Init GPIO for the MOSFET used to connect/disconnect the O2 sensor.
     printf("Setting GPIO6 (board pin 9) to control sensor connect transistor.\n");
     gpio_init(XISTOR_PIN);
     gpio_set_dir(XISTOR_PIN, GPIO_OUT);
     gpio_pull_down(XISTOR_PIN); // Default to transistor off = sensor disconnected.
 
+    // Init the I2C which goes to the OLED and BME280.
     const uint32_t baudrate = 400000;
     printf("Initializing I2C on GPIOs SDA=%d, SCL=%d, %d baud.\n", PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, baudrate);
-    // I2C is "open drain", pull ups to keep signal high when no data is being
-    // sent
+    // I2C is "open drain", pull ups to keep signal high when no data is being sent
     i2c_init(i2c_default, baudrate);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-    /* gpio_set_drive_strength(PICO_DEFAULT_I2C_SDA_PIN, GPIO_DRIVE_STRENGTH_4MA); */
-    /* gpio_set_drive_strength(PICO_DEFAULT_I2C_SCL_PIN, GPIO_DRIVE_STRENGTH_4MA); */
 
+    // Now initialize the OLED display.
     printf("Initializing OLED display across I2C\n");
     // run through the complete initialization process
     oled_init();
     oled_clear_display();
 
+    // Get a reading off the opamp when the O2 sensor is disconnected.  This gives us our "zero" voltage reading.
     // Get the zero voltage offset.
+    // Turn the transistor low which disconnects the O2 sesnsor. (It should be anyway at turn on.)
+    gpio_put(XISTOR_PIN, 0);
+    // Print friendly messages.
     printf("Reading zero voltage level on ADC.  Wait 10 secs...\n");
     sprintf(display_str, "Cal ADC zero level.");
     oled_render_string(0,0,display_str);
     sprintf(display_str, "Wait 10 seconds...");
     oled_render_string(0,1,display_str);
-    // Turn the transistor low. (It should be anyway at turn on.)
-    gpio_put(XISTOR_PIN, 0);
-    // Read 10 values of zero reading.
-    for(i=0; i < ZEROSEQUENCELENGTH; i++){
-        zerolevelsequence[i] = adc_read();
-        printf("Zero level raw value %d of %d: 0x%03x\n", i+1, ZEROSEQUENCELENGTH, zerolevelsequence[i]);
+    // Read a whole buffer of ADC readings.
+    for(i=0; i < ADCSEQUENCELENGTH; i++){
+        adcsequence[i] = adc_read();
+        printf("Zero level raw value %d of %d: 0x%03x\n", i+1, ADCSEQUENCELENGTH, adcsequence[i]);
         sleep_ms(100);
     }
-    zerolevel = mean(zerolevelsequence, ZEROSEQUENCELENGTH);
-    printf("Mean zerolevel is 0x%03x.\n", zerolevel);
-    // Turn the transistor high. (It should be anyway at turn on.)
+    // Average the buffer to get the average zero level.
+    zerolevel = mean(adcsequence, ADCSEQUENCELENGTH);
+    printf("Mean zerolevel is %f.\n", zerolevel);
+    // Turn the transistor high to reconnect the O2 sensor again.
     gpio_put(XISTOR_PIN, 1);
 
+    // Refresh the display.
     oled_clear_display();
 
-    // configure BMP280
+    // configure BME280
     bmp280_init();
 
-    // retrieve fixed compensation params
+    // retrieve fixed compensation params from the BME280 which it needs to make temp adjustments and the like.
     struct bmp280_calib_param params;
     bmp280_get_calib_params(&params);
-
     int32_t raw_temperature;
     int32_t raw_pressure;
     int32_t temperature;
@@ -105,28 +125,27 @@ int main() {
 
     uint8_t current_display_line = 0;
 
-    while (1) {
+    // We take one reading from the O2 sensor and then fill the buffer with that value averaging has sensible values from the start.
+    adcposition = 0;
+    result = adc_read();
+    // We start with the first reading filling the buffer.  As we get more readings the average will get better.
+    for(adcposition=0; adcposition < ADCSEQUENCELENGTH; adcposition++){
+        adcsequence[adcposition] = result;
+    }
 
+    while (1) {
+        // Reset our display to the first line.
+        printf("\n");
         current_display_line = 0;
 
-        // Read the ADC.
-        result = adc_read();
-        voltage = (result-zerolevel) * conversion_factor;
-        O2pct = voltage / VOLTSPERPERCENTO2;
-        // Send the ADC measurement to the serial port.
-        printf("ADC reading: 0x%03x, Voltage: %0.3f V, O2 = %0.2f%%\n", result, voltage, O2pct);
-
-        // Send the ADC measurement to the OLED.
-        /* sprintf(display_str, "ADC reading: 0x%03x", result); */
-        /* oled_render_string(0,current_display_line++,display_str); */
-        sprintf(display_str, "Voltage: %0.3f V", voltage);
-        oled_render_string(0,current_display_line++,display_str);
-        sprintf(display_str, "O2: %0.2f%%", O2pct);
-        oled_render_string(0,current_display_line++,display_str);
-
+        // read the temperature and pressure from the BME280.
         bmp280_read_raw(&raw_temperature, &raw_pressure); 
-        temperature = bmp280_convert_temp(raw_temperature, &params);
+        // Temperature is an int recording hundredths of a degree.
+        temperature = bmp280_convert_temp(raw_temperature, &params); 
+        // Pressure is an int recording Pa.
         pressure = bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
+
+        // Print them out to the serial and OLED.
         printf("Pressure = %.3f kPa\n", pressure / 1000.f);
         printf("Temp. = %.2f C\n", temperature / 100.f);
         sprintf(display_str, "Pressure = %.3f kPa\n", pressure / 1000.f);
@@ -134,6 +153,34 @@ int main() {
         sprintf(display_str, "Temp. = %.2f C\n", temperature / 100.f);
         oled_render_string(0,current_display_line++,display_str);
 
+        // Read the O2 sensor.
+        result = adc_read();
+        // Add to the ring buffer.
+        adcsequence[adcposition++] = result;
+        if(adcposition >= ADCSEQUENCELENGTH){
+            adcposition=0;
+        }
+        // Average the buffer to get the current ADC result.
+        result = mean(adcsequence, ADCSEQUENCELENGTH);
+        // Convert it to a voltage.
+        voltage = (result-zerolevel) * conversion_factor;
+        // Convert the voltage to an O2 percentage.
+        O2pct = voltage / VOLTSPERPERCENTO2;
+        // Calculate how many degrees from 20C. e.g. 23 -> 3.
+        tempcorrection = ((temperature/100.f)-20);
+        // The value changes 3% every 10 degrees so /10.
+        tempcorrection /= 10;
+        // tempcorrection is now a percentage change which needs to be added to the O2 reading.
+        O2pct *= 1 + (tempcorrection/100);
+        
+        // Send the ADC measurement to the serial port.
+        printf("ADC reading: %f, Voltage: %0.3f V, O2 = %0.2f%%\n", result, voltage, O2pct);
+
+        // Send the ADC measurement to the OLED.
+        sprintf(display_str, "Voltage: %0.3f V", voltage);
+        oled_render_string(0,current_display_line++,display_str);
+        sprintf(display_str, "O2: %0.2f%%", O2pct);
+        oled_render_string(0,current_display_line++,display_str);
         // We read every second.
         sleep_ms(1000);
 
